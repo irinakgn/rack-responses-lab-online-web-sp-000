87844ead5e57650277c69ae5f0f4718d0f7106d0
Irina Kagan <irinagamb@gmail.com>
2019-07-08 13:50:06 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,20 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+     time =Time.new
+
+   resp.write time.hour
+
+   if time <= 12
+     resp.write "Good Morning"
+   else
+     resp.write "Good Afternoon"
+   end
+
+   resp.finish
+ end
+
+end
+
